1d75befd59c5ec560253b65e000041e01680f52a
Evgeniy Ermakov <iinegve_25@mail.ru>
2022-12-28 21:23:53 +0300

    feat: add calendar module

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,5 +1,13 @@
 import './css/owfont-regular.css'
 import './css/style.css'
 import Time from './modules/time/time'
+import Calendar from './modules/calendar/calendar';
+
+const locale =  {
+    en: 'en-us',
+    ru: 'ru-ru'
+}
+
 
 Time('.time');
+Calendar('.date', locale.ru);
