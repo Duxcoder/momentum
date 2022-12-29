5b033b1d046beb53d567d93627627757574b69c2
Evgeniy Ermakov <iinegve_25@mail.ru>
2022-12-29 18:30:19 +0300

    refactor: replace on recursive setTimeout

--- momentum/src/modules/time/time.js
+++ momentum/src/modules/time/time.js
@@ -1,15 +1,16 @@
 const Time = (classWrapper) => {
     const wrapper = document.querySelector(classWrapper);
     const addingZeroFirst = (num) => num < 10 ? `0${num}` : num;
+
     const timeOn = () => {
         const date = new Date();
         const hours = date.getHours(),
-              min = date.getMinutes(),
-              sec = date.getSeconds();
+                min = date.getMinutes(),
+                sec = date.getSeconds();
         wrapper.textContent = `${addingZeroFirst(hours)}:${addingZeroFirst(min)}:${addingZeroFirst(sec)}`;
+        setTimeout(timeOn, 1000);
     }
-    timeOn();
-    setInterval(timeOn, 1000);
+    setTimeout(timeOn)
 }
 
 export default Time
