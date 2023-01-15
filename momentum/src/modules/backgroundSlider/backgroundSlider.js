cf2afb642369eab4e3e0cd37d1c9919a0fc4bd73
Evgeniy Ermakov <iinegve_25@mail.ru>
2023-01-15 21:58:29 +0300

    fix: fix background module

--- momentum/src/modules/backgroundSlider/backgroundSlider.js
+++ momentum/src/modules/backgroundSlider/backgroundSlider.js
@@ -1,21 +1,36 @@
 import timesOfDay from "../greetings/timesOfDay"
 import addingZeroFirst from "../time/addingZeroFirst"
 
-const BackgroundSlider = ({classWrapper, classArrowNext, classArrowPrev}) => {
+const BackgroundSlider = ({classWrapper, classArrowNext, classArrowPrev, sourceImages}) => {
      
     const path = "https://raw.githubusercontent.com/Duxcoder/stage1-tasks/assets/images"
     let slideNumberNow = 0;
-    const randomNumber = () => {
-        return Math.ceil((Math.random() * 19.9)) 
+    const randomNumber = (maxNumber) => {
+        return Math.ceil((Math.random() * maxNumber)) 
     }
     const time = timesOfDay('en').replace('Good ', '');
-    const firstNumber = addingZeroFirst(randomNumber());
+    const firstNumber = addingZeroFirst(randomNumber(19.9));
+    let images = [];
 
-    const Render = (number) => {
-        const url = `url("${path}/${time}/${number}.jpg")`
+    const createUrl = (source, number, arr) => {
+        switch (source) {
+            case 'github': return `${path}/${time}/${number}.jpg`
+            case 'unsplash': return `${arr[number - 1][0]}`
+            case 'flickr': return `${arr[number - 1][0]}`
+            case 'unsplashThumb': return `${arr[number - 1][1]}`
+            case 'flickrThumb': return `${arr[number - 1][1]}`
+            default: return `${path}/${time}/${number}.jpg`
+        }
+    }
+
+    const Render = (source, number, arr) => {
+        const url = `url(${createUrl(source, number, arr)})`
         const img = document.createElement('img');
-        img.onload = () => document.body.style.backgroundImage = url; // по загрузке img отобразится фон
-        img.src = `${path}/${time}/${number}.jpg`;
+        img.onload = () => { 
+            document.body.style.backdropFilter = 'blur(0px)'
+            document.body.style.backgroundImage = url; // по загрузке img отобразится фон
+        }
+        img.src = createUrl(source, number, arr);
         slideNumberNow = number;
     }
 
@@ -24,14 +39,20 @@ const BackgroundSlider = ({classWrapper, classArrowNext, classArrowPrev}) => {
           arrowPrev = document.querySelector(classArrowPrev);
 
     const clickNext = () => {
-        const number = +slideNumberNow + 1;
-        if (number > 20) return Render(addingZeroFirst(1))
-        return Render(addingZeroFirst(number))
+        if (sourceImages === 'github') {
+            const number = +slideNumberNow + 1;
+            if (number > 20) return Render(sourceImages, addingZeroFirst(1), images)
+            return Render(sourceImages, addingZeroFirst(number), images)
+        }
+        return Render(sourceImages, addingZeroFirst(randomNumber(20)), images)
     }
     const clickPrev = () => {
-        const number = +slideNumberNow - 1;
-        if (number < 1) return Render(addingZeroFirst(20))
-        return Render(addingZeroFirst(number))
+        if (sourceImages === 'github') {
+            const number = +slideNumberNow - 1;
+            if (number < 1) return Render(sourceImages, 20, images)
+            return Render(sourceImages, addingZeroFirst(number), images)
+        }
+        return Render(sourceImages, addingZeroFirst(randomNumber(20)), images)
     }
 
     wrapper.addEventListener('click', (e) => {
@@ -39,7 +60,21 @@ const BackgroundSlider = ({classWrapper, classArrowNext, classArrowPrev}) => {
         if (e.target === arrowPrev) clickPrev()
     })
 
-    Render(firstNumber);
 
+    let url = `https://api.unsplash.com/search/photos?page=${randomNumber(500)}&orientation=landscape&query=${time}-nature&per_page=20`
+    fetch(url, {
+        headers: {
+            Authorization: 'Client-ID GDoYYGQvzZ1r_y22PViJEMUeWI3i6aqIRxMkQT9aGm8'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(item => {
+            images.push([item.urls.regular, item.urls.thumb])
+        })
+    })
+    .then(() => {
+        Render(sourceImages, firstNumber, images);
+    })
 }
 export default BackgroundSlider
