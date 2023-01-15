038ff1a0be36862ddd6e2f50f29c41e8ebabd663
Evgeniy Ermakov <iinegve_25@mail.ru>
2023-01-15 21:51:15 +0300

    fix: fix options

--- momentum/webpack.config.js
+++ momentum/webpack.config.js
@@ -42,6 +42,10 @@ module.exports = {
             test: /\.(png|jpg|svg|gif)$/,
             type: 'asset/resource',
           },
+          {
+            test: /\.(ogg|mp3|wav|mpe?g)$/i,
+            loader: 'file-loader',
+        }
         ]
       }
 }
